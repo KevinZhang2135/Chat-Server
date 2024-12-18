85c1d719891c65bf89680307be0db329e7f7f5ca
Kevin <kevinbczhang@gmail.com>
2024-12-17 23:33:10 -0500

    Class-ifying Everything

--- src/Client.java
+++ src/Client.java
@@ -1,68 +1,158 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.SocketException;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Arguments required: <multicast-host> <port-number>");
-            return;
+    private final String username;
+    private final InetAddress hostAddress;
+    private final int port;
+
+    private class ClientThread extends Thread {
+        private final Socket socket;
+        private boolean isRunning;
+
+        /**
+         * Initializes a client thread to communicate with a corresponding server thread via the
+         * specified socket
+         * 
+         * @param socket The server socket to listen for
+         */
+        public ClientThread(Socket socket) {
+            this.socket = socket;
+            isRunning = true;
         }
 
-        // Extracts host address and port number form args
-        InetAddress hostAddress;
-        int port = Integer.parseInt(args[1]);
-        try {
-            hostAddress = InetAddress.getByName(args[0]);
+        /**
+         * Continuously listens for messages from the server and displays it them for the user
+         */
+        @Override
+        public void run() {
+            // Reads group chat from server
+            try (BufferedReader serverInput =
+                    new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            return;
+                while (isRunning) {
+                    String response = serverInput.readLine();
+                    if (isRunning && response != null) {
+                        System.out.println("\r" + response);
+                        System.out.print("You: ");
+                    }
+                }
+
+            } catch (SocketException e) {
+                System.out.println("Exited string entered");
+
+            } catch (Exception e) {
+                System.out.println("Safely caught in thread");
+                e.printStackTrace();
+            }
         }
 
-        // Runs thread
+        /**
+         * Terminates the thread and ceases to receive messages from the server
+         */
+        public void kill() {
+            isRunning = false;
+        }
+    }
+
+    /**
+     * 
+     * Initializes the client with the specified username, host address, and port
+     *
+     * @param username The specified username of the client
+     * @param hostAddress The host address of the server
+     * @param port The port used by the server
+     */
+    public Client(String username, InetAddress hostAddress, int port) {
+        this.username = username;
+        this.hostAddress = hostAddress;
+        this.port = port;
+    }
+
+    /**
+     * <p>
+     * Connects to chat server socket and begins exchanging messages.
+     * </p>
+     * 
+     * <p>
+     * A client thread receives and displays incoming messages from other users while outbound
+     * messages from the user are handled by the server thread to be boardcasted.
+     * </p>
+     */
+    public void connectToServer() {
+        Scanner scanner = new Scanner(System.in);
         try (Socket socket = new Socket(hostAddress, port)) {
-            // Reads text input from local machine
-            Scanner scanner = new Scanner(System.in);
-
-            // Gets username
-            System.out.print("Enter name: ");
-            String username = scanner.nextLine();
-
-            // Sets up thread
+            // Output to server
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            // Sets up thread to receive messages from other users
             ClientThread clientThread = new ClientThread(socket);
             clientThread.start();
 
             // Listens for user input
             String message;
-            while (true) {
+            while (clientThread.isAlive()) {
                 System.out.print("You: ");
                 message = scanner.nextLine();
 
                 // User leaves server
-                if (message.equals(Server.EXIT_STRING)) 
-                    break;
-                
-                output.println(String.format("%s (%s): %s ", username, new Date().toString(), message));
+                if (message.equals(Server.EXIT_STRING)) {
+                    clientThread.kill();
+
+                } else {
+                    String timestamp = new Date().toString();
+                    message = String.format("%s (%s): %s ", username, timestamp, message);
+                    output.println(message);
+                }
+
+
             }
 
-            scanner.close();
-            
             // Displays leave message to other users
             output.println(username + " has left.");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        catch (Exception e) {
+            System.out.println("Safely caught in client");
+            e.printStackTrace();
+
+        } finally {
+            scanner.close();
+        }
     }
 
-    public static void backspace(int times) {
-        for (int i = 0; i < times; i++) 
-            System.out.print("\b \b");
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Arguments required: <username> <multicast-host> <port-number>");
+            return;
+        }
+
+        // Extracts username, host address, and port number, from args
+        String username = args[0];
+        InetAddress hostAddress;
+        int port;
+
+        try {
+            hostAddress = InetAddress.getByName(args[1]);
+            port = Integer.parseInt(args[2]);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Creates and connects client to server
+        Client client = new Client(username, hostAddress, port);
+        client.connectToServer();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -5,6 +8,100 @@ import java.util.ArrayList;
 
 public class Server {
     public static final String EXIT_STRING = "exit";
+
+    private int port;
+    private ArrayList<ServerThread> serverThreads;
+
+    private class ServerThread extends Thread {
+        private Socket socket;
+        private PrintWriter output;
+
+        /**
+         * Initializes a thread at a specified socket to listen for client messages
+         * 
+         * @param socket The specified socket to listen for
+         */
+        public ServerThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        /**
+         * Continuously listens from its client and broadcasts messages to all other clients
+         */
+        @Override
+        public void run() {
+            // Reads input from client
+            try (BufferedReader input =
+                    new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                // Gets output from client
+                output = new PrintWriter(socket.getOutputStream(), true);
+
+                // Continuously listens for client input
+                while (true) {
+                    String outputString = input.readLine();
+
+                    // Removes thread from server pool when the user enters the exit
+                    // string
+                    if (outputString == null) {
+                        serverThreads.remove(this);
+                        break;
+                    }
+
+                    outputString = outputString.strip().toLowerCase();
+                    printToClients(outputString, socket);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Displays client input to all other clients on the chat server
+         * 
+         * @param outputString
+         */
+        private void printToClients(String outputString, Socket socket) {
+            for (ServerThread thread : serverThreads)
+                thread.output.println(outputString);
+        }
+    }
+
+    /**
+     * Initializes a chat server with the specified port and no currently running threads
+     * 
+     * @param port The specified port number used by the server socket
+     */
+    public Server(int port) {
+        this.port = port;
+        serverThreads = new ArrayList<>();
+    }
+
+    /**
+     * Starts listening for new clients joining the server and faciliates communication between them
+     */
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            InetAddress localHost = InetAddress.getLocalHost();
+            System.out.println("Server running on " + localHost);
+
+            // Continuously listens for new socket connections
+            while (true) {
+                // Blocks until new connection is made
+                Socket socket = serverSocket.accept();
+                ServerThread serverThread = new ServerThread(socket);
+
+                serverThreads.add(serverThread);
+                serverThread.start();
+
+                System.out.println("Number of clients: " + serverThreads.size());
+                System.out.println(serverThreads + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -15,26 +112,8 @@ public class Server {
         // Extracts port number form args
         int port = Integer.parseInt(args[0]);
 
-        // Runs server
-        ArrayList<ServerThread> serverThreadPool = new ArrayList<>();
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server running on " + InetAddress.getLocalHost());
-
-            // Continuously listens for new socket connections
-            while (true) {
-                Socket socket = serverSocket.accept(); // blocks until new connection is made
-                ServerThread serverThread = new ServerThread(socket, serverThreadPool);
-
-                serverThreadPool.add(serverThread);
-                serverThread.start();
-                
-                System.out.println("Number of clients: " + serverThreadPool.size());
-                System.out.println(serverThreadPool + "\n");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // Creates and runs server
+        Server server = new Server(port);
+        server.run();
     }
 }
